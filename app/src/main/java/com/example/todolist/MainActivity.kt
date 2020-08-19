65b0770fff3bcba915d90e48605a9ace4b3e82ce
TenBort <kozubzkiy@gmail.com>
2020-08-19 17:26:49 +0300

    try used tabs

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_to_do_list.*
 
-class MainActivity : AppCompatActivity() {
+class  MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
--- app/src/main/java/com/example/todolist/ToDoListFragment.kt
+++ app/src/main/java/com/example/todolist/ToDoListFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.fragment_to_do_list.*
 
 
 class ToDoListFragment : Fragment() {
@@ -15,6 +19,10 @@ class ToDoListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_to_do_list, container, false)
+
+        val fragmentAdapter = MyPagerAdapter(parentFragmentManager)
+        viewpager.adapter = fragmentAdapter
+        tabs.setupWithViewPager(viewpager)
 
     }
 
