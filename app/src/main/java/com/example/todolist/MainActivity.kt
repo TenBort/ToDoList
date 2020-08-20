d2515050102e0ea806fd5a7313ae70c2f04b703a
TenBort <kozubzkiy@gmail.com>
2020-08-20 20:21:14 +0300

    20.08.20

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.fragment_to_do_list.*
+
 
 class  MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
     }
 }
