0a7df659cbcb2908ceb4f43165b8b93391403f05
TenBort <kozubzkiy@gmail.com>
2020-08-30 20:00:03 +0300

    CardView rework

--- app/src/main/java/com/example/todolist/adapters/ToDoRecyclerAdapter.kt
+++ app/src/main/java/com/example/todolist/adapters/ToDoRecyclerAdapter.kt
@@ -5,12 +5,14 @@ import android.service.quicksettings.Tile
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.view.get
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.model.DataItem
 import com.example.todolist.R
+import com.squareup.picasso.Picasso
 
 
 class ToDoRecyclerAdapter() :
@@ -21,75 +23,21 @@ class ToDoRecyclerAdapter() :
         DataItem(
             "27.08.2020",
             "StartTest",
-            "Hoh man"
+            "Hoh man",
+            "https://i.pinimg.com/originals/87/af/ef/87afef76100d0b704ca5b6039468a736.jpg"
+
         ),
         DataItem(
             "07.12.1998",
             "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
+            "yyyyyyyyyyyyyyyyyyyyyyy",
+            "https://i.pinimg.com/originals/87/af/ef/87afef76100d0b704ca5b6039468a736.jpg"
         ),
         DataItem(
             "07.12.1998",
             "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
-        ),
-        DataItem(
-            "07.12.1998",
-            "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
-        ),
-        DataItem(
-            "07.12.1998",
-            "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
-        )
-        ,
-        DataItem(
-            "07.12.1998",
-            "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
-        )
-        ,
-        DataItem(
-            "07.12.1998",
-            "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
-        )
-        ,
-        DataItem(
-            "07.12.1998",
-            "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
-        )
-        ,
-        DataItem(
-            "07.12.1998",
-            "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
-        )
-        ,
-        DataItem(
-            "07.12.1998",
-            "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
-        )
-        ,
-        DataItem(
-            "07.12.1998",
-            "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
-        )
-        ,
-        DataItem(
-            "07.12.1998",
-            "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
-        )
-        ,
-        DataItem(
-            "07.12.1998",
-            "Shit its Jason BORNing",
-            "yyyyyyyyyyyyyyyyyyyyyyy"
+            "yyyyyyyyyyyyyyyyyyyyyyy",
+            "https://i.pinimg.com/originals/87/af/ef/87afef76100d0b704ca5b6039468a736.jpg"
         )
     )
 
@@ -108,12 +56,13 @@ class ToDoRecyclerAdapter() :
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         if (position < 20) {
             holder.cardView.setCardBackgroundColor(colors[position])
-        }else{
+        } else {
             holder.cardView.setCardBackgroundColor(colors.random())
         }
 
         holder.textContainer.text = list[position].date
         holder.textContainer2.text = list[position].name
+        holder.picassoHelper(list[position].img)
     }
 
     override fun getItemCount() = list.size
@@ -124,7 +73,15 @@ class ToDoRecyclerAdapter() :
         var textContainer = view.findViewById<TextView>(R.id.textViewItem)
         var textContainer2 = view.findViewById<TextView>(R.id.textViewItem2)
         var cardView = view.findViewById<CardView>(R.id.card_view)
+        var img = view.findViewById<ImageView>(R.id.imageView)
 
+
+        fun picassoHelper(url: String) {
+
+            Picasso.with(itemView.context).load(url).into(img)
+
+
+        }
     }
 }
 
--- app/src/main/java/com/example/todolist/model/DataItem.kt
+++ app/src/main/java/com/example/todolist/model/DataItem.kt
@@ -3,4 +3,4 @@ package com.example.todolist.model
 import java.io.FileDescriptor
 import java.util.*
 
-data class DataItem (val date:String, val name:String, val description:String)
+data class DataItem (val date:String, val name:String, val description:String,val img:String)
