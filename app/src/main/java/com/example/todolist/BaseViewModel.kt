4956993615f600f0acc695a246a4b49df530083c
TenBort <kozubzkiy@gmail.com>
2020-09-08 15:48:36 +0300

    First DataBase

--- app/src/main/java/com/example/todolist/BaseViewModel.kt
+++ app/src/main/java/com/example/todolist/BaseViewModel.kt
@@ -2,29 +2,25 @@ package com.example.todolist
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.todolist.adapters.ToDoRecyclerAdapter
 import com.example.todolist.model.Task
 
 
 class BaseViewModel : ViewModel(){
+//    val adaterRecycler = ToDoRecyclerAdapter()
+//
+//    val list = MutableLiveData<List<Task>>()
+//
+//    fun saveTask (task: Task){
+//        //adaterRecycler.list = list.value!!
+//        val items = mutableListOf<Task>()
+//        items.add(task)
+//
+//        list.value = items
+//    }
 
-    val list = MutableLiveData<List<Task>>()
+    val list = MutableLiveData<MutableList<Task>>(mutableListOf())
 
-    fun saveTask (task: Task){
-
-        val items = mutableListOf<Task>()
-        items.add(task)
-
-        list.value = items
-
-
-
-
-
-
-
-
-
-
-    }
+    fun saveTask(task: Task) = list.value!!.add(task)
 
 }
--- app/src/main/java/com/example/todolist/fragments/FirstTabFragment.kt
+++ app/src/main/java/com/example/todolist/fragments/FirstTabFragment.kt
@@ -51,6 +51,5 @@ class FirstTabFragment : Fragment() {
             adapterRecycler.list = it
             adapterRecycler.notifyDataSetChanged()
         })
-
     }
 }
